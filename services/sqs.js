e8348e0ff2a2cd21beba469b54d7c8335026b3d5
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-05-14 14:09:21 -0400

    SQS send message method

--- services/sqs.js
+++ services/sqs.js
@@ -4,9 +4,21 @@ const { addAwsPromiseRetries } = require('../common');
 
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
-// TODO
+function promiseSendMessage(method, body) {
+    let maxRetries = 5;
+    let params = {
+        QueueUrl: sqsQueue,
+        MessageBody: body,
+        MessageAttributes: {
+            Method: {
+                DataType: 'String',
+                StringValue: method
+            }
+        }
+    };
+    return addAwsPromiseRetries(() => sqs.sendMessage(params).promise(), maxRetries);
+}
 
 module.exports = {
-    // TODO
-    //externalName : internalName
+    sendMessage : promiseSendMessage
 }
