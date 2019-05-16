b6e4cac978a9a0e632a92897f18321e056986dad
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-05-16 11:54:00 -0400

    Clean up api

--- api.js
+++ api.js
@@ -17,7 +17,7 @@ async function callAndLog(stage, promise) {
 }
 
 async function apiCreate(body, callerEmail, cognitoUsername) {
-    let methodName = 'create';
+    const methodName = 'create';
     validate.createBody(body);
 
     let dappName = validate.cleanName(body.DappName);
@@ -26,59 +26,51 @@ async function apiCreate(body, callerEmail, cognitoUsername) {
     let web3URL = body.Web3URL;
     let guardianURL = body.GuardianURL;
 
-    try {
-        await validate.createAllowed(dappName, cognitoUsername, callerEmail);
+    await validate.createAllowed(dappName, cognitoUsername, callerEmail);
 
-        let sqsMessageBody = {
-            Method: methodName,
-            DappName: dappName
-        };
+    let sqsMessageBody = {
+        Method: methodName,
+        DappName: dappName
+    };
 
-        await callAndLog('Put DynamoDB Item', dynamoDB.putItem(dappName, callerEmail, abi, addr, web3URL, guardianURL));
-        await callAndLog('Send SQS Message', sqs.sendMessage(methodName, JSON.stringify(sqsMessageBody)));
+    await callAndLog('Put DynamoDB Item', dynamoDB.putItem(dappName, callerEmail, abi, addr, web3URL, guardianURL));
+    await callAndLog('Send SQS Message', sqs.sendMessage(methodName, JSON.stringify(sqsMessageBody)));
 
-        let responseBody = {
-            method: methodName,
-            message: "Dapp generation successfully initialized!  Check your URL in about 5 minutes."
-        };
-        return responseBody;
-    } catch (err) {
-        throw err;
-    }
+    let responseBody = {
+        method: methodName,
+        message: "Dapp generation successfully initialized!  Check your URL in about 5 minutes."
+    };
+    return responseBody;
 }
 
 async function apiRead(body, callerEmail) {
-    let methodName = 'read';
+    const methodName = 'read';
     validate.readBody(body);
 
     let dappName = validate.cleanName(body.DappName);
 
+    let dbItem = await callAndLog('Get DynamoDB Item', dynamoDB.getItem(dappName));
+
+    let outputItem;
     try {
-        const dbItem = await callAndLog('Get DynamoDB Item', dynamoDB.getItem(dappName));
-
-        let outputItem = null;
-        try {
-            await validate.readAllowed(dbItem, callerEmail);
-            outputItem = dynamoDB.toApiRepresentation(dbItem.Item);
-        } catch (err) {
-            console.log("Read permission denied. Returning empty object.", err);
-            outputItem = {};
-        }
-
-        let itemExists = Boolean(outputItem.DappName);
-        let responseBody = {
-            method: methodName,
-            exists: itemExists,
-            item: outputItem
-        };
-        return responseBody;
+        await validate.readAllowed(dbItem, callerEmail);
+        outputItem = dynamoDB.toApiRepresentation(dbItem.Item);
     } catch (err) {
-        throw err;
+        console.log("Read permission denied. Returning empty object.", err);
+        outputItem = {};
     }
+
+    let itemExists = Boolean(outputItem.DappName);
+    let responseBody = {
+        method: methodName,
+        exists: itemExists,
+        item: outputItem
+    };
+    return responseBody;
 }
 
 async function apiUpdate(body, callerEmail) {
-    let methodName = 'update';
+    const methodName = 'update';
     validate.updateBody(body);
 
     let dappName = validate.cleanName(body.DappName);
@@ -96,76 +88,63 @@ async function apiUpdate(body, callerEmail) {
         return responseBody;
     }
 
-    try {
-        let dbItem = await validate.updateAllowed(dappName, callerEmail);
+    let dbItem = await validate.updateAllowed(dappName, callerEmail);
 
-        let updateAttrs = {
-            Abi: abi,
-            ContractAddr: addr,
-            Web3URL: web3URL,
-            GuardianURL: guardianURL
-        };
-        let sqsMessageBody = {
-            Method: methodName,
-            DappName: dappName
-        };
+    let updateAttrs = {
+        Abi: abi,
+        ContractAddr: addr,
+        Web3URL: web3URL,
+        GuardianURL: guardianURL
+    };
+    let sqsMessageBody = {
+        Method: methodName,
+        DappName: dappName
+    };
 
-        await callAndLog("Set DynamoDB Item State Building And Update Attributes", dynamoDB.setStateBuildingWithUpdate(dbItem, updateAttrs));
-        await callAndLog('Send SQS Message', sqs.sendMessage(methodName, JSON.stringify(sqsMessageBody)));
+    await callAndLog("Set DynamoDB Item State Building And Update Attributes", dynamoDB.setStateBuildingWithUpdate(dbItem, updateAttrs));
+    await callAndLog('Send SQS Message', sqs.sendMessage(methodName, JSON.stringify(sqsMessageBody)));
 
-        let responseBody = {
-            method: methodName,
-            message: "Your Dapp was successfully updated! Allow 5 minutes for rebuild, then check your URL."
-        };
-        return responseBody;
-    } catch (err) {
-        throw err; 
-    }
+    let responseBody = {
+        method: methodName,
+        message: "Your Dapp was successfully updated! Allow 5 minutes for rebuild, then check your URL."
+    };
+    return responseBody;
 }
 
 async function apiDelete(body, callerEmail) {
-    let methodName = 'delete';
+    const methodName = 'delete';
     validate.deleteBody(body);
 
     let dappName = validate.cleanName(body.DappName);
 
-    try {
-        let dbItem = await validate.deleteAllowed(dappName, callerEmail);
+    let dbItem = await validate.deleteAllowed(dappName, callerEmail);
 
-        let sqsMessageBody = {
-            Method: methodName,
-            DappName: dappName
-        };
+    let sqsMessageBody = {
+        Method: methodName,
+        DappName: dappName
+    };
 
-        await callAndLog("Set DynamoDB Item State Deleting", dynamoDB.setStateDeleting(dbItem));
-        await callAndLog('Send SQS Message', sqs.sendMessage(methodName, JSON.stringify(sqsMessageBody)));
+    await callAndLog("Set DynamoDB Item State Deleting", dynamoDB.setStateDeleting(dbItem));
+    await callAndLog('Send SQS Message', sqs.sendMessage(methodName, JSON.stringify(sqsMessageBody)));
 
-        let responseBody = {
-            method: methodName,
-            message: "Your Dapp was successfully deleted."
-        };
-        return responseBody;
-    } catch (err) {
-        throw err;
-    }
-
+    let responseBody = {
+        method: methodName,
+        message: "Your Dapp was successfully deleted."
+    };
+    return responseBody;
 }
 
 async function apiList(callerEmail) {
-    let methodName = 'list';
+    const methodName = 'list';
 
-    try {
-        let ddbResponse = await callAndLog('List DynamoDB Items', dynamoDB.getByOwner(callerEmail));
-        let outputItems = ddbResponse.Items.map(item => dynamoDB.toApiRepresentation(item));
-        let responseBody = {
-            method: methodName,
-            count: ddbResponse.Count,
-            items: outputItems
-        };
-        return responseBody;
-    } catch (err) {
-        throw err;
-    }
+    let ddbResponse = await callAndLog('List DynamoDB Items', dynamoDB.getByOwner(callerEmail));
+    let outputItems = ddbResponse.Items.map(item => dynamoDB.toApiRepresentation(item));
+    let responseBody = {
+        method: methodName,
+        count: ddbResponse.Count,
+        items: outputItems
+    };
+    return responseBody;
 }
 
 module.exports = {
